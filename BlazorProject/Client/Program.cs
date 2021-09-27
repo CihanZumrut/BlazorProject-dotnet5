f939272b624b39692da94bfbd37f7a5940da55a2
CİHAN ZÜMRÜT <cihanzumrut@hotmail.com>
2021-09-27 17:57:02 +0300

    Toast Notifications with Blazored.Toast

--- BlazorProject/Client/Program.cs
+++ BlazorProject/Client/Program.cs
@@ -1,13 +1,10 @@
 using BlazorProject.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Blazored.Toast;
 
 namespace BlazorProject.Client
 {
@@ -18,6 +15,7 @@ namespace BlazorProject.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
